e08901537a1dea9fb3c121b82a360cc09dc9a9a6
Kiran Aatkar <kiranaatkar@gmail.com>
2025-04-06 12:16:39 +0100

    testing api url

--- film-review/src/services/client.ts
+++ film-review/src/services/client.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+console.log("leg: ", process.env.REACT_APP_DB_API_URL);
+
 const apiClient = axios.create({
   baseURL: process.env.REACT_APP_DB_API_URL,
   headers: { "Content-Type": "application/json" },
